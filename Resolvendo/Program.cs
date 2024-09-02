b37aea8e2b34544a09ed6a163adc0557e2223b34
CaioSix <caio-cavalcanti@hotmail.com>
2024-09-02 15:47:28 -0300

    adicionando arquivos

---
+++ Resolvendo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe um número para verificar se pertence à sequência de Fibonacci: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (PertenceFibonacci(numero))
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceFibonacci(int num)
+    {
+        int a = 0;
+        int b = 1;
+
+        while (a < num)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return a == num;
+    }
+}
---
+++ Resolvendo/Valores.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Valores
+{
+    static void Main()
+    {
+        Console.Write("Informe uma string para verificar a existência da letra 'a': ");
+        string input = Console.ReadLine();
+
+        int ocorrencias = ContarOcorrenciasDeA(input);
+
+        if (ocorrencias > 0)
+        {
+            Console.WriteLine($"A letra 'a' (maiúscula ou minúscula) ocorre {ocorrencias} vezes na string.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' (maiúscula ou minúscula) não ocorre na string.");
+        }
+    }
+
+    static int ContarOcorrenciasDeA(string texto)
+    {
+        int contador = 0;
+
+        foreach (char c in texto)
+        {
+            if (c == 'a' || c == 'A')
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
